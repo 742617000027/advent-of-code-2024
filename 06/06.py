ed0571c8a68e27833906a2af3b15960893163042
742617000027 <15521878+742617000027@users.noreply.github.com>
2024-12-11 10:14:26 +0100

    Add day 06 part 2

--- 06/06.py
+++ 06/06.py
@@ -4,12 +4,32 @@ import utils
 from utils import deepcopy
 
 
-def walk(board, start_pos, dir_idx, find_loops=False, reference_visits=None):
+def find_potential_loops(board):
+    visited, _ = walk(board, start_pos, dir_idx)
+    obstacles_to_check = set()
+    loops = 0
+
+    for locs in visited.values():
+
+        for ny, nx in locs:
+
+            if board[ny][nx] == "." and (ny, nx) != start_pos:
+                obstacles_to_check.add((ny, nx))
+
+    for y, x in obstacles_to_check:
+        local_board = deepcopy(board)
+        local_board[y][x] = "#"
+        _, has_loop = walk(local_board, start_pos, dir_idx, find_loops=True)
+        loops += has_loop
+
+    return loops
+
+
+def walk(board, start_pos, dir_idx, find_loops=False):
     Y, X = len(board), len(board[0])
     dy, dx = utils.DIRS[dir_idx]
     y, x = start_pos
     visited = defaultdict(set)
-    loops = set()
 
     while 0 <= (ny := dy + y) < Y and 0 <= (nx := dx + x) < X:
 
@@ -18,29 +38,14 @@ def walk(board, start_pos, dir_idx, find_loops=False, reference_visits=None):
             dy, dx = utils.DIRS[dir_idx]
             continue
 
-        if find_loops and obstacle_at_new_pos_would_start_loop(board, visited, (y, x), (ny, nx), dir_idx):
-            loops.add((ny, nx))
-
-        if reference_visits is not None:
-            if (y, x) in reference_visits and (ny, nx) in reference_visits[(y, x)]:
-                return visited, None, True
+        if find_loops:
             if (y, x) in visited and (ny, nx) in visited[(y, x)]:
-                return visited, None, True
+                return visited, True
 
         visited[(y, x)].add((ny, nx))
         y, x = ny, nx
 
-    visited[(y, x)].add((ny, nx))
-
-    return visited, loops, False
-
-
-def obstacle_at_new_pos_would_start_loop(board, path, pos, new_pos, dir_idx):
-    modified_board = deepcopy(board)
-    ny, nx = new_pos
-    modified_board[ny][nx] = "#"
-    _, _, would_start_loop = walk(modified_board, pos, dir_idx, reference_visits=path)
-    return would_start_loop
+    return visited, False
 
 
 def process_puzzle_input(puzzle_input):
@@ -65,13 +70,12 @@ if __name__ == "__main__":
     # Part 1
     timer.start()
     board, start_pos, dir_idx = process_puzzle_input(puzzle_input)
-    visited, _, _ = walk(board, start_pos, dir_idx, find_loops=False)
+    visited, _ = walk(board, start_pos, dir_idx)
     print(len(visited))
     timer.stop()  # 4.31ms
 
     # Part 2
-    # timer.start()
-    # board, start_pos, dir_idx = process_puzzle_input(puzzle_input)
-    # _, loops, _ = walk(board, start_pos, dir_idx, find_loops=True)
-    # print(len(loops))
-    # timer.stop()  # 15.05ms
+    timer.start()
+    board, start_pos, dir_idx = process_puzzle_input(puzzle_input)
+    print(find_potential_loops(board))
+    timer.stop()  # 21811.14ms
